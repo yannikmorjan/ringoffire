7ed0ce424380deec7a6f718633d8567403228b4f
yannikmorjan <yannik.morjan@web.de>
2023-04-04 22:33:09 +0200

    added card logic

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent {
+export class GameComponent implements OnInit{
+  pickCardAnimation = false;
+  currentCard: string = '';
+  game: Game;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+  }
+
+  takeCard() {
+    if(!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      this.pickCardAnimation = true;
+  
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard);
+        this.pickCardAnimation = false;
+      }, 1000);
+    }
+  }
 
 }
